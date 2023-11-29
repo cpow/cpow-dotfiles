b0507d6d0c161e2f5762521e77e5d3da0120da6d
Christopher Power <chris.power@underdogfantasy.com>
2023-11-29 14:35:32 -0500

    make comments italic

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -1,6 +1,9 @@
 require("catppuccin").setup({
   flavour = "mocha",
   transparent_background = true,
+  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments
+  },
 })
 
 vim.o.termguicolors = true
